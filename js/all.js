123fbd2a26dbf5fdfb7da9f875ae903499cefb7d
zh-kaiii <bthd0404@gmail.com>
2021-02-07 16:49:19 +0800

    Update all.js

--- js/all.js
+++ js/all.js
@@ -3,4 +3,22 @@ $(document).ready(function () {
         e.preventDefault();
         $('body').toggleClass('burger');
     });
+
+    //滑動到一定位置再顯示 top 鍵
+    var $pc = $(window);    
+    $pc.scroll(function (){
+        if ($pc.scrollTop() > 100){    
+            $(".top").css('opacity','100');
+        }   else{
+            $(".top").css('opacity','0')
+        };
+    });
+
+    //回到頂部
+    $('.top').click(function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        },1000);
+    });
 });
